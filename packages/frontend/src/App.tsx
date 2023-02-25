773be6f4eb850a389380ddcb56d144b40b4cf622
Giovanni Buracci <giovanni.buracci@hotmail.com>
2023-02-25 15:06:22 +0100

    route system stub

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import {Fragment, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import { Button } from "@chakra-ui/button";
@@ -7,34 +7,60 @@ import { useDispatch, useSelector } from "react-redux";
 import { testSelectors } from "./store/testStore/testStore.selector";
 import { testActions } from "./store/testStore/testStore.action";
 import Navbar from "./components/Navbar";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import React from 'react';
 
 function App() {
-  const visible = useSelector(testSelectors.getVisible);
-  console.log("visible", visible);
-  const dispatch = useDispatch();
+    const visible = useSelector(testSelectors.getVisible);
+    const dispatch = useDispatch();
+    const [isLogged, setIsLogged] = useState<boolean>(false)
+    const navigate = useNavigate();
 
-  return (
-    <>
-      <Navbar />
-
-      <div className="App">
-        <Text fontSize="6xl" as="b">
-          Pro.io
-        </Text>
-        <Text fontSize="2xl" color="blue.theme">
-          Homepage
-        </Text>
-        <button
-          onClick={() => {
-            dispatch(testActions.testAction(!visible));
-          }}
-        >
-          Cliccami
-        </button>
-        {visible ? <h1> ciao sono visibile</h1> : null}
-      </div>
-    </>
-  );
+    return (
+        <>
+            {
+                isLogged ? (
+                    <Routes>
+                        <Route path={'/*'} element={<>
+                            <div className="App">
+                                <Navbar logOut={() => {
+                                    setIsLogged(false);
+                                    navigate('/')
+                                }}
+                                        isLogged={true} />
+                                <Text fontSize="6xl" as="b">
+                                    Pro.io
+                                </Text>
+                                <Text fontSize="2xl" color="blue.theme">
+                                    Homepage
+                                </Text>
+                                <button
+                                    onClick={() => {
+                                        dispatch(testActions.testAction(!visible));
+                                    }}
+                                >
+                                    Cliccami
+                                </button>
+                                {visible ? <h1> ciao sono visibile</h1> : null}
+                            </div>
+                        </>}>
+                        </Route>
+                    </Routes>
+                ) : (
+                    <>
+                        <Navbar isLogged={false} />
+                        <Routes>
+                            <Route path={'/'} element={<>
+                                HOMEPAGE PER UTENTE NON LOGGATO
+                            </>} />
+                            <Route path={'/login'} element={<><Button onClick={() => setIsLogged(true)}>LOGGATI</Button>
+                            </>} />
+                        </Routes>
+                    </>
+                )
+            }
+        </>
+    );
 }
 
 export default App;
