7d9d401b52f5a3fe10d6b912a5b79b5649bf30f4
GMN177 <gimmy177@icloud.com>
2023-04-16 16:33:39 +0200

    Integrated new services + nginx
    -Integrated new services in Docker Compose
    - Added Nginx as reverse Proxy

--- packages/backEnd/gameService/src/configs/database.js
+++ packages/backEnd/gameService/src/configs/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 
 function connectToDatabase() {
     mongoose.set('strictQuery', true)
-    return mongoose.connect(process.env.DB_URI)
+    return mongoose.connect(process.env.GAME_DB_URI)
 }
 
 module.exports = {connectToDatabase}
