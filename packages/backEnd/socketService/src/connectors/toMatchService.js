ea0a21a93a01c1700820b4f080987e76adf44b89
GMN177 <gimmy177@icloud.com>
2023-06-18 23:27:41 +0200

    Fix Match Call + Debug

--- packages/backEnd/socketService/src/connectors/toMatchService.js
+++ packages/backEnd/socketService/src/connectors/toMatchService.js
@@ -1,7 +1,7 @@
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args))
 
 const getMatch = async (matchId) => {
-    const response = await fetch('http://matchservice:4000/match/' + matchId);
+    const response = await fetch('http://matchservice:4000/api/matches/' + matchId);
     const data = await response.json();
     return data;
 };
--- packages/backEnd/socketService/src/handlers/index.js
+++ packages/backEnd/socketService/src/handlers/index.js
@@ -5,48 +5,54 @@ const { gameStateService } = require('../handlers/game.js');
 let matches = {};
 
 const onConnection = async (socket) => {
-    socket.join(socket.handshake.query.matchId);
+    try {
+        console.log("New connection: " + socket.id);
+        
+        socket.join(socket.handshake.query.matchId);
     
-    console.log("New client connected in room: " + socket.handshake.query.matchId);
+        console.log("New client connected in room: " + socket.handshake.query.matchId);
 
-    let match;
-    if (matches.containsKey(socket.handshake.query.matchId)) {
-        match = matches[socket.handshake.query.matchId];
-    } else {
-        match = await getMatch(socket.handshake.query.matchId);
-        matches[socket.handshake.query.matchId] = match;
+        let match;
+        if (matches.containsKey(socket.handshake.query.matchId)) {
+            match = matches[socket.handshake.query.matchId];
+        } else {
+            match = await getMatch(socket.handshake.query.matchId);
+            matches[socket.handshake.query.matchId] = match;
+        }
+        console.log(match);
+
+        socket.emit("newState", {
+            state: gameStateService.getSnapshot(),
+            nextEvents: gameStateService.getSnapshot().machine.states[gameStateService.getSnapshot().value].events
+        });
+    
+        socket.on("READY", (msg) => {
+            sendEventAndEmitNewState(io, gameStateService, {
+                type: "READY",
+                value: msg.player
+            }, socket.handshake.query.matchId);
+        });
+
+        socket.on("PLAY", (msg) => {
+            sendEventAndEmitNewState(io, gameStateService, {
+                type: "PLAY",
+                player: msg.player,
+                value: msg.i
+            }, socket.handshake.query.matchId);
+        });
+    
+        socket.on("RESET", () => {
+            sendEventAndEmitNewState(io, gameStateService, {
+                type: "RESET"
+            }, socket.handshake.query.matchId);
+        });
+
+        socket.on("disconnect", () => {
+            console.log("Client disconnected");
+        });
+    } catch (err) {
+        console.log(err);
     }
-    console.log(match);
-
-    socket.emit("newState", {
-        state: gameStateService.getSnapshot(),
-        nextEvents: gameStateService.getSnapshot().machine.states[gameStateService.getSnapshot().value].events
-    });
-    
-    socket.on("READY", (msg) => {
-        sendEventAndEmitNewState(io, gameStateService, {
-            type: "READY",
-            value: msg.player
-        }, socket.handshake.query.matchId);
-    });
-
-    socket.on("PLAY", (msg) => {
-        sendEventAndEmitNewState(io, gameStateService, {
-            type: "PLAY",
-            player: msg.player,
-            value: msg.i
-        }, socket.handshake.query.matchId);
-    });
-    
-    socket.on("RESET", () => {
-        sendEventAndEmitNewState(io, gameStateService, {
-            type: "RESET"
-        }, socket.handshake.query.matchId);
-    });
-
-    socket.on("disconnect", () => {
-        console.log("Client disconnected");
-    });
 };
 
 const onConnectionError = (err) => {
