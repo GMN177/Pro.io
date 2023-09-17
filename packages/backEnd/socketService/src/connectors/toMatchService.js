58bfa6d42000419ce40d49ef61d6f2338f5c83c3
GMN177 <gimmy177@icloud.com>
2023-09-17 15:31:28 +0200

    Fix log

--- packages/backEnd/socketService/src/connectors/toMatchService.js
+++ packages/backEnd/socketService/src/connectors/toMatchService.js
@@ -3,7 +3,7 @@ const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch
 const getMatch = async (matchId) => {
     const response = await fetch('http://matchservice:4000/api/matches/' + matchId);
     const data = await response.json();
-    console.log('response:' + data);
+    console.log('response:', data);
     return data.data.message;
 };
 
