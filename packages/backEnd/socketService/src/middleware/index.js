d78b8d44f2ceff1807914adb4a7419de6f9616b0
GMN177 <gimmy177@icloud.com>
2023-10-01 17:11:39 +0200

    Add log

--- packages/backEnd/socketService/src/middleware/index.js
+++ packages/backEnd/socketService/src/middleware/index.js
@@ -1,12 +1,13 @@
 const verifyToken = (socket, next) => {
     const token = socket.handshake.query.token;
+    console.log(socket.handshake)
+    console.log(socket.handshake.query)
     console.log(token);
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
         if (err) {
             console.log(err);
             return next(new Error('Authentication error'));
         }
-        //socket.decodedToken = decoded;
         socket.playerId = decoded.id;
         next();
     });
