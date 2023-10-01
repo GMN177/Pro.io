b185d4de300cf735d2faf042ff89ccdc3ceef765
GMN177 <gimmy177@icloud.com>
2023-10-01 17:27:38 +0200

    Fix socket auth

--- packages/backEnd/socketService/src/middleware/index.js
+++ packages/backEnd/socketService/src/middleware/index.js
@@ -1,14 +1,21 @@
+const jwt = require('jsonwebtoken')
+
 const verifyToken = (socket, next) => {
-    const token = socket.handshake.auth.token;
-    console.log(token);
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-        if (err) {
-            console.log(err);
-            return next(new Error('Authentication error'));
-        }
-        socket.playerId = decoded.id;
-        next();
-    });
+    try {
+        const token = socket.handshake.auth.token;
+        console.log(token);
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+            if (err) {
+                console.log(err);
+                next(new Error('Authentication error'));
+            }
+            socket.playerId = decoded.id;
+            next();
+        });
+    } catch (err) {
+        console.log(err);
+        next(new Error('Authentication error'));
+    }
 };
 
 module.exports = {
