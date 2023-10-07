00f10cb8265237aaadf3b0812094b8ac650bc0f5
Giovanni Buracci <giovanni.buracci@hotmail.com>
2023-10-07 11:59:24 +0200

    tictactoe setup

--- packages/frontend/src/pages/Game.jsx
+++ packages/frontend/src/pages/Game.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {useParams} from "react-router-dom";
+import {TicTacToe} from "@/components/games/TicTacToe";
 
 export const Game = () => {
 
@@ -7,6 +8,20 @@ export const Game = () => {
 
     console.log('game', game)
     console.log('matchId', matchId)
+
+    const renderGame = () => {
+        if(!game) {
+            return null
+        }
+        switch(game) {
+            case 'TicTacToe': {
+                return <TicTacToe />
+            }
+            default: {
+                return <>Game not supported</>
+            }
+        }
+    }
     return (
         <>
 
