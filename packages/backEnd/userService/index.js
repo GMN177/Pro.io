e09ce509e240b5fd44ae2d9936a90a1ec3d9fb64
GMN177 <gimmy177@icloud.com>
2023-10-22 15:51:36 +0200

    Add morgan logger middleware

--- packages/backEnd/userService/index.js
+++ packages/backEnd/userService/index.js
@@ -1,27 +1,35 @@
 const express = require('express');
-const cors = require('cors')
-const logger = require('./src/middlewares/logMiddleware')
+const cors = require('cors');
+const morgan = require('morgan');
 const userServiceRouter = require('./src/services/userService');
-const authServiceRouter =  require('./src/services/authService')
-const {connectToDatabase} = require('./src/configs/database')
+const authServiceRouter = require('./src/services/authService');
+const {
+    connectToDatabase
+} = require('./src/configs/database');
 
 const app = express();
 
 app.use(cors({
     origin: '*'
-}))
+}));
 
 app.use(express.json());
-app.use(logger)
+
+app.use(morgan((tokens, req, res) => JSON.stringify({
+    method: tokens.method(req, res),
+    url: tokens.url(req, res),
+    status: Number.parseFloat(tokens.status(req, res)),
+    content_length: tokens.res(req, res, 'content-length'),
+    response_time: Number.parseFloat(tokens['response-time'](req, res)),
+    request_body: req.body
+})));
 
 app.use('/api/users', userServiceRouter);
-app.use('/api/auth', authServiceRouter)
-
-
+app.use('/api/auth', authServiceRouter);
 
 connectToDatabase()
-.then(() => {
-    let port = process.env.SERVER_PORT || 4000
-    app.listen(port, () => console.log(`SYSTEM UP AND RUNNING ON PORT ${port}!`))
-})
-.catch((err) => console.log(err))
+    .then(() => {
+        let port = process.env.SERVER_PORT || 4000
+        app.listen(port, () => console.log(`SYSTEM UP AND RUNNING ON PORT ${port}!`))
+    })
+    .catch((err) => console.log(err));
