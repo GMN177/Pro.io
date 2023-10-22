e88b62a2df88183e52e3095fc6798e0cc32ade34
GMN177 <gimmy177@icloud.com>
2023-10-23 00:12:23 +0200

    Fix startup order + await

--- packages/backEnd/logAggregatorService/src/index.js
+++ packages/backEnd/logAggregatorService/src/index.js
@@ -2,28 +2,32 @@ require('dotenv').config();
 const amqp = require('amqplib');
 const logger = require('./utils/logger');
 
-try {
-    const connection = await amqp.connect(process.env.RABBITMQ_URI);
+const listen = async () => {
+    try {
+        const connection = await amqp.connect(process.env.RABBITMQ_URI);
 
-    const channel = await connection.createChannel();
+        const channel = await connection.createChannel();
 
-    process.once("SIGINT", async () => {
-        await channel.close();
-        await connection.close();
-    });
+        process.once("SIGINT", async () => {
+            await channel.close();
+            await connection.close();
+        });
 
-    await channel.assertQueue(process.env.LOGS_QUEUE, {
-        durable: false
-    });
+        await channel.assertQueue(process.env.LOGS_QUEUE, {
+            durable: false
+        });
 
-    await channel.consume(
-        process.env.LOGS_QUEUE,
-        (message) => {
-            logger.log(message.content.toString());
-        }, {
-            noAck: true
-        }
-    );
-} catch (err) {
-    console.log(err);
+        await channel.consume(
+            process.env.LOGS_QUEUE,
+            (message) => {
+                logger.log(message.content.toString());
+            }, {
+                noAck: true
+            }
+        );
+    } catch (err) {
+        console.log(err);
+    }
 }
+
+listen();
