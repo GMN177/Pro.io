d53e3103d804efeb961c87ecabc6ce4e2af6e75d
GMN177 <gimmy177@icloud.com>
2023-10-26 20:55:19 +0200

    Fix retrieve match

--- packages/backEnd/ticTacToeService/src/connectors/toMatchService.js
+++ packages/backEnd/ticTacToeService/src/connectors/toMatchService.js
@@ -2,6 +2,12 @@ const fetch = (...args) => import('node-fetch').then(({
     default: fetch
 }) => fetch(...args))
 
+const getMatch = async (matchId) => {
+    const response = await fetch('http://matchservice:4000/api/matches/' + matchId);
+    const data = await response.json();
+    return data.data.message;
+};
+
 const endMatch = async (matchId, body) => {
     const response = await fetch('http://matchservice:4000/api/matches/' + matchId + "/endMatch", {
         method: 'PATCH',
