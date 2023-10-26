88c5c1be69623abc1198a02dd0a16de3f45ee494
GMN177 <gimmy177@icloud.com>
2023-10-26 21:58:30 +0200

    Catch errors

--- packages/backEnd/ticTacToeService/src/connectors/toMatchService.js
+++ packages/backEnd/ticTacToeService/src/connectors/toMatchService.js
@@ -3,19 +3,27 @@ const fetch = (...args) => import('node-fetch').then(({
 }) => fetch(...args))
 
 const getMatch = async (matchId) => {
-    const response = await fetch('http://matchservice:4000/api/matches/' + matchId);
-    const data = await response.json();
-    return data.data.message;
+    try {
+        const response = await fetch('http://matchservice:4000/api/matches/' + matchId);
+        const data = await response.json();
+        return data.data.message;
+    } catch (err) {
+        return null;
+    }
 };
 
 const endMatch = async (matchId, body) => {
-    const response = await fetch('http://matchservice:4000/api/matches/' + matchId + "/endMatch", {
-        method: 'PATCH',
-        body: JSON.stringify(body),
-        headers: { 'Content-Type': 'application/json' }
-    });
-    const data = await response.json();
-    return data.data.message;
+    try {
+        const response = await fetch('http://matchservice:4000/api/matches/' + matchId + "/endMatch", {
+            method: 'PATCH',
+            body: JSON.stringify(body),
+            headers: { 'Content-Type': 'application/json' }
+        });
+        const data = await response.json();
+        return data.data.message;
+    } catch (err) {
+        return null;
+    }
 };
             
 
