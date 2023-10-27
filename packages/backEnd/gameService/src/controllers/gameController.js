0accbbf40989be7af16f3920051be307d710ca06
GMN177 <gimmy177@icloud.com>
2023-10-27 21:53:27 +0200

    Cleanup game service

--- packages/backEnd/gameService/src/controllers/gameController.js
+++ packages/backEnd/gameService/src/controllers/gameController.js
@@ -1,103 +1,90 @@
 const Game = require("../models/game");
-const { default: mongoose } = require("mongoose");
+const {
+    default: mongoose
+} = require("mongoose");
 const responses = require("../models/responses");
 
 async function getAllGames() {
-  try {
     const games = await Game.find({});
 
-    return response = responses.genericSuccessResponse(200, games);
-  } catch (err) {
-    throw new Error(err.message);
-  }
+    return responses.genericSuccessResponse(200, games);
 }
 
 async function getGame(id) {
-  console.log("SONO DENTRO")
-  if (!mongoose.isValidObjectId(id)) {
-    return responses.INVALID_ID;
-  }
-  try {
-    console.log("ID", id)
-    const game = await Game.findById(id);
-    console.log(game)
-    if (game == null) {
-      return responses.INVALID_ID;
+    if (!mongoose.isValidObjectId(id)) {
+        return responses.INVALID_ID;
     }
+
+    const game = await Game.findById(id);
+
+    if (game == null) {
+        return responses.INVALID_ID;
+    }
+
     let gameToRetrieve = {
-      id: game._id,
-      name: game.name,
-      duration: game.description,
-      playersNumber: game.playersNumber
+        id: game._id,
+        name: game.name,
+        duration: game.description,
+        playersNumber: game.playersNumber
     };
 
-    console.log("GAMERET", gameToRetrieve)
-
-    return response = responses.genericSuccessResponse(200, gameToRetrieve);
-  } catch (err) {
-    throw new Error(err.message);
-  }
+    return responses.genericSuccessResponse(200, gameToRetrieve);
 }
 
 async function getGameByName(name) {
-  try {
-    const game = await Game.findOne({ name: name });
+    const game = await Game.findOne({
+        name: name
+    });
+
     if (game == null) {
-      return responses.INVALID_NAME;
+        return responses.INVALID_NAME;
     }
+
     let gameToRetrieve = {
-      id: game._id,
-      name: game.name,
-      description: game.description,
-      playersNumber: game.playersNumber
+        id: game._id,
+        name: game.name,
+        description: game.description,
+        playersNumber: game.playersNumber
     };
 
-    return response = responses.genericSuccessResponse(200, gameToRetrieve);
-  } catch (err) {
-    throw new Error(err.message);
-  }
+    return responses.genericSuccessResponse(200, gameToRetrieve);
 }
 
 async function updateGame(gameName, gameDescription, gamePlayersNumber, id) {
-  if (!mongoose.isValidObjectId(id)) {
-    return responses.INVALID_ID;
-  }
-  try {
+    if (!mongoose.isValidObjectId(id)) {
+        return responses.INVALID_ID;
+    }
+
     let gameUpdated = {
-      name: gameName,
-      description: gameDescription,
-      playersNumber: gamePlayersNumber,
+        name: gameName,
+        description: gameDescription,
+        playersNumber: gamePlayersNumber,
     };
-    let ret = await Game.findByIdAndUpdate({ _id: id }, gameUpdated, {
-      new: true,
-      overwrite: true,
+
+    await Game.findByIdAndUpdate({
+        _id: id
+    }, gameUpdated, {
+        new: true,
+        overwrite: true,
     });
+
     return responses.UPDATE_SUCCESS;
-  } catch (err) {
-    throw new Error(err.message);
-  }
 }
 
 async function createGame(gameName, gameDescription, gamePlayersNumber) {
-  try {
-    const gameToAdd = await Game.create({
-      name: gameName,
-      description: gameDescription,
-      playersNumber: gamePlayersNumber,
+    await Game.create({
+        name: gameName,
+        description: gameDescription,
+        playersNumber: gamePlayersNumber,
     });
-    return response = responses.genericSuccessResponse(200, "Game added");
-  } catch (err) {
-    if (err.code === 11000) throw new Error(err.code);
-    else {
-      throw new Error(err.message);
-    }
-  }
+
+    return responses.genericSuccessResponse(200, "Game added");
 }
 
 module.exports = {
-  getAllGames,
-  getGame,
-  getGameByName,
-  updateGame,
-  createGame,
+    getAllGames,
+    getGame,
+    getGameByName,
+    updateGame,
+    createGame,
 };
--- packages/backEnd/gameService/src/services/gameService.js
+++ packages/backEnd/gameService/src/services/gameService.js
@@ -1,71 +1,89 @@
 const express = require("express");
 const jsend = require("jsend");
+const logger = require("../utils/logger");
 const gameController = require("../controllers/gameController");
 
 const router = express.Router();
 
 // get all games from database
 router.get("/", async (req, res) => {
-  try {
-    let games = await gameController.getAllGames();
-    return res.status(games.status).send(games.response);
-  } catch (err) {
-    return res.status(500).send(jsend.error({ message: err.message }));
-  }
+    try {
+        let games = await gameController.getAllGames();
+        return res.status(games.status).send(games.response);
+    } catch (err) {
+        logger.error(err.message);
+        return res.status(500).send(jsend.error({
+            message: err.message
+        }));
+    }
 });
 
 // get single game from database.
 router.get("/:id", async (req, res) => {
-  try {
-    let game = await gameController.getGame(req.params.id);
-    return res.status(game.status).send(game.response);
-  } catch (err) {
-    return res.status(500).send(jsend.error({ message: err.message }));
-  }
+    try {
+        let game = await gameController.getGame(req.params.id);
+        return res.status(game.status).send(game.response);
+    } catch (err) {
+        logger.error(err.message);
+        return res.status(500).send(jsend.error({
+            message: err.message
+        }));
+    }
 });
 
 // get single game by name from database.
 router.get("/game/:name", async (req, res) => {
-  try {
-    let game = await gameController.getGameByName(req.params.name);
-    return res.status(game.status).send(game.response);
-  } catch (err) {
-    return res.status(500).send(jsend.error({ message: err.message }));
-  }
+    try {
+        let game = await gameController.getGameByName(req.params.name);
+        return res.status(game.status).send(game.response);
+    } catch (err) {
+        logger.error(err.message);
+        return res.status(500).send(jsend.error({
+            message: err.message
+        }));
+    }
 });
 
 // modify game.
 router.put("/:id", async (req, res) => {
-  try {
-    const gameUpdated = await gameController.updateGame(
-      req.body.gameName,
-      req.body.gameDescription,
-      req.body.gamePlayersNumber,
-      req.params.id
-    );
-    return res.status(gameUpdated.status).send(gameUpdated.response);
-  } catch (err) {
-    return res.status(500).send(jsend.error({ message: err.message }));
-  }
+    try {
+        const gameUpdated = await gameController.updateGame(
+            req.body.gameName,
+            req.body.gameDescription,
+            req.body.gamePlayersNumber,
+            req.params.id
+        );
+        return res.status(gameUpdated.status).send(gameUpdated.response);
+    } catch (err) {
+        logger.error(err.message);
+        return res.status(500).send(jsend.error({
+            message: err.message
+        }));
+    }
 });
 
 router.post("/", async (req, res) => {
-  try {
-    const gameToAdd = await gameController.createGame(
-      req.body.gameName,
-      req.body.gameDescription,
-      req.body.gamePlayersNumber
-    );
-    return res.status(gameToAdd.status).send(gameToAdd.response);
-  } catch (err) {
-    if (Number(err.message) === 11000) {
-      return res
-        .status(409)
-        .send(jsend.error({ message: "Duplicate key error" }));
-    } else {
-      return res.status(500).send(jsend.error({ message: err.message }));
+    try {
+        const gameToAdd = await gameController.createGame(
+            req.body.gameName,
+            req.body.gameDescription,
+            req.body.gamePlayersNumber
+        );
+        return res.status(gameToAdd.status).send(gameToAdd.response);
+    } catch (err) {
+        logger.error(err.message);
+        if (err.code === 11000) {
+            return res
+                .status(409)
+                .send(jsend.error({
+                    message: "Duplicate key error"
+                }));
+        } else {
+            return res.status(500).send(jsend.error({
+                message: err.message
+            }));
+        }
     }
-  }
 });
 
 module.exports = router;
